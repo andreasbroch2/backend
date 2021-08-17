66918eb36294ebb43a11b1e6f2c951a74ba8e4f9
andreasbroch2 <andreas@gaiamadservice.dk>
2021-08-17 12:32:52 +0200

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/www/')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
