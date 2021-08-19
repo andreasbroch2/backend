e105fd7866906e9ffd55c45d217c58dca0041dbc
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2021-08-19 18:21:54 +0200

    url

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/opskrifter', views.opskrifter, name='opskrifter'),
+    path('opskrifter/', views.opskrifter, name='opskrifter'),
 ]
