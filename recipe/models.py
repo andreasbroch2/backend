597b7f73a0870fc0e45b245742e9dbce83af9b1d
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2021-08-20 22:27:00 +0200

    sd

--- recipe/models.py
+++ recipe/models.py
@@ -10,8 +10,8 @@ class RecipeManager(models.Manager):
 class Recipe(models.Model):
     name = models.CharField(max_length=120)
     type = models.CharField(max_length=120)
-    ingredients = models.JSONField()
-    method = models.JSONField()
+    ingredients = models.JSONField(default=dict)
+    method = models.JSONField(default=dict)
     summary = models.TextField(default="This is cool")
     date_created = models.DateField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
