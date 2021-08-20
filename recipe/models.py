2eec124e8ed7d882b5713dc9b885dc8f659f8121
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2021-08-20 17:29:21 +0200

    dssd

--- recipe/models.py
+++ recipe/models.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     name = models.CharField(max_length=120)
-    type = models.TextField()
+    type = models.CharField(max_length=120)
     ingredients = models.JSONField()
     method = models.JSONField()
     summary = models.TextField(default="This is cool")
     date_created = models.DateField(default=timezone.now)
-    author = models.TextField(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
